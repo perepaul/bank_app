b94d1befa5e6faca8772adbc167ade1f0e0f286f
Perekpo Paulinus <perepau15@gmail.com>
2020-03-12 17:20:15 +0100

    changed email address

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
     'phone' => env('APP_PHONE', '1-404-9477-119'),
-    'email' => env('APP_EMAIL', 'service@ftbank.com'),
+    'email' => env('APP_EMAIL', 'service@5th-3rd.com'),
     'address' => env('APP_ADDRESS', '3344 Peachtree Rd NE 800, Atlanta, GA 30326 USA .'),
 
     /*
