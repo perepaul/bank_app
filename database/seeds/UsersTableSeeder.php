c5cb5884f75315b80fa5ec25ec8bc6a91ded913d
Perekpo Paulinus <perepaul5@gmail.com>
2020-04-25 23:50:18 +0100

    changed the email extensions to .org

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,66 +13,66 @@ class UsersTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $account_type = array('Savings','Checking','Recurring Deposit','Fixed Deposit');
+        $account_type = array('Savings', 'Checking', 'Recurring Deposit', 'Fixed Deposit');
         $password = 'secret';
         $users = [
-                        [
-                            'account_id'            =>       $faker->unique()->bankAccountNumber,
-                            'first_name'            =>       $faker->unique()->firstName,
-                            'last_name'             =>       $faker->unique()->lastName,
-                            'middle_name'           =>       $faker->unique()->lastName,
-                            'account_type'          =>       $faker->randomElement($account_type),
-                            'phone_number'          =>       $faker->phoneNumber,
-                            'mother_name'           =>       $faker->firstNameFemale,
-                            'email'                 =>       'admin@5th-3rd.com',
-                            'password'              =>       $password,
-                            'visible_password'      =>       $password,
-                            'gender'                =>       1,
-                            'account_number'        =>       $faker->unique()->bankAccountNumber,
-                            'status'                =>       1,
-                            'is_admin'              =>       1,
-                            'balance'               =>       1000000,
-                            'address'               =>       $faker->streetAddress
-                        ],
-                        [
-                            'account_id'            =>       $faker->unique()->bankAccountNumber,
-                            'first_name'            =>       $faker->unique()->firstName,
-                            'last_name'             =>       $faker->unique()->lastName,
-                            'middle_name'           =>       $faker->unique()->lastName,
-                            'account_type'          =>       $faker->randomElement($account_type),
-                            'phone_number'          =>       $faker->phoneNumber,
-                            'mother_name'           =>       $faker->firstNameFemale,
-                            'email'                 =>       'user1@5th-3rd.com',
-                            'password'              =>       $password,
-                            'visible_password'      =>       $password,
-                            'gender'                =>       1,
-                            'account_number'        =>       $faker->unique()->bankAccountNumber,
-                            'status'                =>       2,
-                            'is_admin'              =>       0,
-                            'balance'               =>       1000000,
-                            'address'               =>       $faker->streetAddress
-                        ],
-                        [
-                            'account_id'            =>       $faker->unique()->bankAccountNumber,
-                            'first_name'            =>       $faker->unique()->firstName,
-                            'last_name'             =>       $faker->unique()->lastName,
-                            'middle_name'           =>       $faker->unique()->lastName,
-                            'phone_number'          =>       $faker->phoneNumber,
-                            'mother_name'           =>       $faker->firstNameFemale,
-                            'account_type'          =>       $faker->randomElement($account_type),
-                            'email'                 =>       'user2@5th-3rd.com',
-                            'password'              =>       $password,
-                            'visible_password'      =>       $password,
-                            'gender'                =>       3,
-                            'account_number'        =>       $faker->unique()->bankAccountNumber,
-                            'status'                =>       1,
-                            'is_admin'              =>       0,
-                            'balance'               =>       10000000,
-                            'address'               =>       $faker->streetAddress
-                        ]
+            [
+                'account_id'            =>       $faker->unique()->bankAccountNumber,
+                'first_name'            =>       $faker->unique()->firstName,
+                'last_name'             =>       $faker->unique()->lastName,
+                'middle_name'           =>       $faker->unique()->lastName,
+                'account_type'          =>       $faker->randomElement($account_type),
+                'phone_number'          =>       $faker->phoneNumber,
+                'mother_name'           =>       $faker->firstNameFemale,
+                'email'                 =>       'admin@5th-3rd.org',
+                'password'              =>       $password,
+                'visible_password'      =>       $password,
+                'gender'                =>       1,
+                'account_number'        =>       $faker->unique()->bankAccountNumber,
+                'status'                =>       1,
+                'is_admin'              =>       1,
+                'balance'               =>       1000000,
+                'address'               =>       $faker->streetAddress
+            ],
+            [
+                'account_id'            =>       $faker->unique()->bankAccountNumber,
+                'first_name'            =>       $faker->unique()->firstName,
+                'last_name'             =>       $faker->unique()->lastName,
+                'middle_name'           =>       $faker->unique()->lastName,
+                'account_type'          =>       $faker->randomElement($account_type),
+                'phone_number'          =>       $faker->phoneNumber,
+                'mother_name'           =>       $faker->firstNameFemale,
+                'email'                 =>       'user1@5th-3rd.org',
+                'password'              =>       $password,
+                'visible_password'      =>       $password,
+                'gender'                =>       1,
+                'account_number'        =>       $faker->unique()->bankAccountNumber,
+                'status'                =>       2,
+                'is_admin'              =>       0,
+                'balance'               =>       1000000,
+                'address'               =>       $faker->streetAddress
+            ],
+            [
+                'account_id'            =>       $faker->unique()->bankAccountNumber,
+                'first_name'            =>       $faker->unique()->firstName,
+                'last_name'             =>       $faker->unique()->lastName,
+                'middle_name'           =>       $faker->unique()->lastName,
+                'phone_number'          =>       $faker->phoneNumber,
+                'mother_name'           =>       $faker->firstNameFemale,
+                'account_type'          =>       $faker->randomElement($account_type),
+                'email'                 =>       'user2@5th-3rd.org',
+                'password'              =>       $password,
+                'visible_password'      =>       $password,
+                'gender'                =>       3,
+                'account_number'        =>       $faker->unique()->bankAccountNumber,
+                'status'                =>       1,
+                'is_admin'              =>       0,
+                'balance'               =>       10000000,
+                'address'               =>       $faker->streetAddress
+            ]
 
-                    ];
-        foreach($users as $user){
+        ];
+        foreach ($users as $user) {
 
             User::create($user);
         }
