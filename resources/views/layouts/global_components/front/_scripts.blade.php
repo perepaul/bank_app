beb8e8a1de8a5c6f5a2cb0b12ead935ca1520696
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-03-20 10:16:58 +0100

    chat widget updated to tidio

--- resources/views/layouts/global_components/front/_scripts.blade.php
+++ resources/views/layouts/global_components/front/_scripts.blade.php
@@ -1,18 +1,4 @@
 {{-- <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script> --}}
 <script src="{{asset('assets/front/js/core.min.js')}}"></script>
 <script src="{{asset('assets/front/js/script.js')}}"></script>
-<script type='text/javascript' data-cfasync='false'>
-    <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6055b6d8067c2605c0ba78ae/1f17d4gd2';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-</script>
+<script src="//code.tidio.co/vhfcwiabexwoq3i5mrfilmytnu0nzc2g.js" async></script>
