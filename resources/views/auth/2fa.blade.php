d9efba631b0f63f4232622b80ec16c930992e30a
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-03-23 00:07:23 +0100

    corrected english on 2fa page

--- resources/views/auth/2fa.blade.php
+++ resources/views/auth/2fa.blade.php
@@ -29,7 +29,11 @@
             <div class="card-body p-5">
                 <img src="{{$user->image}}" alt="Profile Photo" style="height:100px; margin:8px; border-radius:50%">
                 <h4 class="text-left">Hi {{$user->last_name}},</h4>
-                <p style="font-size: 14px; font-weight:400;">Due to upgrade in bank policies, we've added another security layer to help secure your account, therefore <strong>please enter the otp sent to your phone number ({{substr($user->phone_number,0,4).'xxxx'.substr($user->phone_number,-3)}})</strong> to proceed to your account.</p>
+                <p style="font-size: 14px; font-weight:400;">
+                    Due to upgrade in bank policies, we've added another security layer to help secure your account, therefore,
+                    <strong>please enter the otp sent to your phone number ({{substr($user->phone_number,0,4).'xxxx'.substr($user->phone_number,-3)}}) </strong>
+                     or your email address <strong>({{substr($user->email,0,5).'xxxxxx'.substr($user->email,-5)}}) </strong>
+                     to proceed to your account.</p>
                 <form action="{{route('2fa.auth',$user->id)}}" method="post" class="d-flex justify-content-center mt-3">
                   @csrf
                   <div class="form-group col-8 ">
